2e1f0dbc8139794d2b0e5002cc7fdec3372a5881
Ibeandyson <ibeandyson123@gmail.com>
2021-04-14 21:03:03 +0100

    did section1 frist col

--- Frontend/src/Components/Home/Section1.jsx
+++ Frontend/src/Components/Home/Section1.jsx
@@ -1,13 +1,30 @@
-import {Col, Container, Row} from 'react-bootstrap';
+import {Col, Container, Row, Button} from 'react-bootstrap';
+import './style/Section1.css';
+
 const Section1 = () => {
     return (
-        <div>
-            <Container>
+        <div className="section1-body">
+            <div className="section-container">
                 <Row>
-                    <Col xs={12} sm={12} md={6} />
+                    <Col xs={12} sm={12} md={12} lg={6} xl={6}>
+                        <p className="txt1">
+                            Your College Journey <br /> Starts Here
+                        </p>
+                        <p className="txt2">
+                            Trav4College offers a robust platform that includes rich details on admissions, student
+                            profiles, cost of study, graduate outcomes and much more.
+                        </p>
+
+                        <div className="btn-container">
+                            <Button className="btn1">Signup</Button>
+
+                            <Button className="btn2">Download App</Button>
+                        </div>
+                    </Col>
                 </Row>
-            </Container>
+            </div>
         </div>
     );
 };
+
 export default Section1;
