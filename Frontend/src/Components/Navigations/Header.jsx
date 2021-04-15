5a9860f44f75bf7f81b5e699457433541832b89d
Ibeandyson <ibeandyson123@gmail.com>
2021-04-15 01:26:32 +0100

    fixed

--- Frontend/src/Components/Navigations/Header.jsx
+++ Frontend/src/Components/Navigations/Header.jsx
@@ -5,12 +5,12 @@ import './style/Header.css';
 const Header = () => {
     return (
         <div>
-            <Navbar  fixed="top" collapseOnSelect expand="lg" className="header-bg">
+            <Navbar fixed="top" collapseOnSelect expand="lg" className="header-bg">
                 <Navbar.Brand href="#home" className="logo">
                     <img src={Logo} /> <label className="logo-text">Trav4College</label>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav"  className="mobile">
+                <Navbar.Collapse id="responsive-navbar-nav" className="mobile">
                     <Nav className="mr-auto">
                         <Nav.Link href="#features" className="header-text">
                             College Application
@@ -18,19 +18,15 @@ const Header = () => {
                         <Nav.Link href="#pricing" className="header-text">
                             Scholarships
                         </Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown" className="header-text">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#pricing" className="header-text">
+                            Study Abroad Solutions
+                        </Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#deets" className="login-text">
                             Login
                         </Nav.Link>
-                  
+
                         <Nav.Link eventKey={2} href="#memes">
                             <Button className="btn-get-started">Get started</Button>
                         </Nav.Link>
