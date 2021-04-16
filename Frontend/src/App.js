7a2b624cdf52944542266b67e0e581b763e8f62e
Ibeandyson <ibeandyson123@gmail.com>
2021-04-16 15:06:27 +0100

    made foter

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import {LandingPage} from "./Pages/"
-import {Header} from "./Components/Navigations"
+import {LandingPage} from './Pages/';
+import {Header, Footer} from './Components/Navigations';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-     <LandingPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <LandingPage />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
